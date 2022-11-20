277e747f50a85e025a94f9e937493143b8deff67
Aisha <113794732+ayyshahh1@users.noreply.github.com>
2022-11-20 04:41:04 -0800

    Add files via upload

---
+++ Portfolio/nav.js
@@ -0,0 +1,52 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+  
+    navLinks.forEach((link, index) => {
+      link.addEventListener('click', (e) => {
+        e.preventDefault();
+        burger.classList.toggle('toggle');
+        nav.classList.toggle('nav-active');
+        navLinks.forEach(setStyleLink)
+      })
+    });
+  
+    burger.addEventListener('click', () => {
+      // Toggle Nav
+      nav.classList.toggle('nav-active');
+      // animate navlinks
+      navLinks.forEach(setStyleLink);
+      // burger animation
+      burger.classList.toggle('toggle');
+    });
+  }
+  
+  function setStyleLink(el, index) {
+    if (el.style.animation) {
+      el.style.animation = ''
+    } else {
+      el.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 +0.5}s`;
+    }
+  }
+  navSlide();
+
+
+
+// const navSlide = () => {
+//     const menu = document.querySelector('.menu');
+//     const nav = document.querySelector('nav-link');
+
+//     menu.addEventListener('click', () => {
+//         nav.classList.toggle('nav-active');
+//     });
+// }
+
+// navSlide();
+
+// const app = ()=>{
+//     navSlide();
+//     navSlide();
+//     navSlide();
+//     navSlide();
+// }
